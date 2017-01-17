34432a11756cf691139e5279e9c7a579fd482cb5
ThomasMeijers <thomasmeijers@live.nl>
2017-01-18 00:13:27 +0100

    Work on branches

--- DecisionTree.hs
+++ DecisionTree.hs
@@ -1,5 +1,7 @@
 module DecisionTree where
-  import Data.List (nub, genericLength)
+  import System.Directory (getCurrentDirectory)
+  import qualified Data.Text as T
+  import Data.List (nub, genericLength, transpose, sortBy)
   import Control.Applicative
   import Data.Traversable
   import Data.Foldable hiding (sum)
@@ -9,46 +11,56 @@ module DecisionTree where
   import qualified Data.Traversable ()
   import qualified Control.Applicative ()
 
+  main :: IO ()
+  main = do
+    currentDir <- getCurrentDirectory
+    file       <- readFile (currentDir ++ "/dtree.csv")
+    print $ let (x:xs)  = fmap (T.splitOn $ T.pack ",") $ T.lines $ T.pack file
+                columns = zip x (transpose xs)
+            in build (snd (last columns)) (init columns)
+
   -- | A DecisionTree consists of either a Node or Leaf
   -- The Leaf contains a value for the classification
   -- A Node contains an attribute of the data (a) and a List of Tuple2
   -- where the first element is the predicate to that specific child, the
   -- second element
-  data DecisionTree a = Node a [DecisionTree a]
+  data DecisionTree a = Node a [Branch a]
                       | Leaf a
                       deriving Show
 
-  -- | Instance of the Foldable typeclass for DecisionTree
-  -- Makes it possible to call fold functions
-  instance Foldable DecisionTree where
-      foldMap f (Node a ts) = f a `mappend` foldMap (foldMap f) ts
-      foldMap f (Leaf a)    = f a
+  data Branch a = Branch a (DecisionTree a) deriving Show
 
-  -- | Instance of the Traversable typeclass for DecisionTree
-  -- Makes it possible to traverse over DecisionTrees
-  instance Traversable DecisionTree where
-      traverse f (Leaf a)    = Leaf <$> f a
-      traverse f (Node a ts) = Node <$> f a <*> for ts (traverse f)
-
-  -- | Instance of the Functor typeclass for DecisionTree
-  -- Makes it possible to map over DecisionTrees
-  instance Functor DecisionTree where
-      fmap = liftM
-
-  -- | Instance of the Applicative Functor typeclass for DecisionTree
-  -- Makes it possible to have sequential application over DecisionTrees
-  instance Applicative DecisionTree where
-      pure  = Leaf
-      (<*>) = ap
-
-  -- | Instance of the Monad typeclass for DecisionTree
-  -- Makes it possible to dependent sequencing over DecisionTrees
-  instance Monad DecisionTree where
-      return = pure
-      (Leaf a)    >>= f = f a
-      (Node a ta) >>= f = case f a of
-          (Leaf b)    -> Node b (fmap (>>= f) ta)
-          (Node b tb) -> Node b (tb `mappend` fmap (>>= f) ta)
+  -- -- | Instance of the Foldable typeclass for DecisionTree
+  -- -- Makes it possible to call fold functions
+  -- instance Foldable DecisionTree where
+  --     foldMap f (Node a ts) = f a `mappend` foldMap (foldMap f) ts
+  --     foldMap f (Leaf a)    = f a
+  --
+  -- -- | Instance of the Traversable typeclass for DecisionTree
+  -- -- Makes it possible to traverse over DecisionTrees
+  -- instance Traversable DecisionTree where
+  --     traverse f (Leaf a)    = Leaf <$> f a
+  --     traverse f (Node a ts) = Node <$> f a <*> for ts (traverse f)
+  --
+  -- -- | Instance of the Functor typeclass for DecisionTree
+  -- -- Makes it possible to map over DecisionTrees
+  -- instance Functor DecisionTree where
+  --     fmap = liftM
+  --
+  -- -- | Instance of the Applicative Functor typeclass for DecisionTree
+  -- -- Makes it possible to have sequential application over DecisionTrees
+  -- instance Applicative DecisionTree where
+  --     pure  = Leaf
+  --     (<*>) = ap
+  --
+  -- -- | Instance of the Monad typeclass for DecisionTree
+  -- -- Makes it possible to dependent sequencing over DecisionTrees
+  -- instance Monad DecisionTree where
+  --     return = pure
+  --     (Leaf a)    >>= f = f a
+  --     (Node a ta) >>= f = case f a of
+  --         (Leaf b)    -> Node b (fmap (>>= f) ta)
+  --         (Node b tb) -> Node b (tb `mappend` fmap (>>= f) ta)
 
   -- | Calculates the entropy of a List
   -- applies the lambda over unique elements of xs (by using nub) and sums
@@ -61,5 +73,12 @@ module DecisionTree where
   -- | Calculates the gain of two lists
   gain :: Eq a => [a] -> [a] -> Double
   gain xs ys = entropy xs - sum (fmap (\t -> p t * entropy t) tss)
-    where p t' = genericLength t' / genericLength xs
-          tss = fmap (\x -> fmap fst $ filter ((==) x . snd) $ zip xs ys) (nub ys)
+    where p t'     = genericLength t' / genericLength xs
+          tss      = fmap subset (nub ys)
+          subset x = fmap fst $ filter ((==) x . snd) $ zip xs ys
+
+  build :: Eq a => [a] -> [(a, [a])] -> DecisionTree a
+  build xs tss = Node n (fmap (\b -> Branch b (Leaf b)) (nub ns))
+    where withGain   = fmap (\(t, ts) -> (t, ts, gain xs ts)) tss
+          sorted     = sortBy (\(_, _, g) (_, _, g') -> compare g' g) withGain
+          (n, ns, _) = head sorted
---
+++ notes.md
@@ -0,0 +1,15 @@
+# Notes for Data Mining
+
+## Probability
+
+P(h), the probability that some hypothesis h is true, is called the **prior probability** of h.
+Before we have any evidence, the probability of a person owning a Mac is 0.6 (the evidence
+might be knowing that the person also owns an iPhone).
+
+P(h|d) is called the **posterior probability** of h. After we observe some data d what is the
+probability of h? For example, after we observe that a person owns an iPhone, what is the
+probability of that same person owning a Mac? It is also called **conditional probability**.
+
+**P(D)** is the probability that some training data will be observed.
+
+**P(D|h)** is the probability that some data value holds given the hypothesis.
